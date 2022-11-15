171b9125e0b9e2be44e3e503c008a5f53f9280a8
hyrdbyrd <ilya777grin23@yandex-team.ru>
2022-11-15 09:58:58 +0300

    clean and more

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { ThemeProvider, Switch } from '@gravity-ui/uikit';
+import { ThemeProvider, Switch, Breadcrumbs, Button, LastDisplayedItemsCount, FirstDisplayedItemsCount } from '@gravity-ui/uikit';
 
 import './theme.scss';
-import './App.css';
-
 
 function App() {
   return (
     <ThemeProvider>
-      <Switch />
+      <Switch title="Swch" />
+      <Breadcrumbs
+        lastDisplayedItemsCount={LastDisplayedItemsCount.One}
+        firstDisplayedItemsCount={FirstDisplayedItemsCount.One}
+        items={['b', 'read', 'crumb', 's'].map(text => ({ text, action: () => {} }))}
+      />
+      <Button>Btn</Button>
     </ThemeProvider>
   )
 }
