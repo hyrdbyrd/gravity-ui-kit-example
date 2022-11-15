1c5324c5f2805bf2b288e92c96a76048cf39e5d7
hyrdbyrd <ilya777grin23@yandex-team.ru>
2022-11-15 10:06:05 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { ThemeProvider, Switch, Breadcrumbs, Button, LastDisplayedItemsCount, FirstDisplayedItemsCount } from '@gravity-ui/uikit';
+import {
+    ThemeProvider,
+    Switch,
+    Breadcrumbs,
+    Button,
+    LastDisplayedItemsCount,
+    FirstDisplayedItemsCount,
+} from '@gravity-ui/uikit';
 
 import './theme.scss';
 
 function App() {
-  return (
-    <ThemeProvider>
-      <Switch title="Swch" />
-      <Breadcrumbs
-        lastDisplayedItemsCount={LastDisplayedItemsCount.One}
-        firstDisplayedItemsCount={FirstDisplayedItemsCount.One}
-        items={['b', 'read', 'crumb', 's'].map(text => ({ text, action: () => {} }))}
-      />
-      <Button>Btn</Button>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider>
+            <Switch title="Swch" />
+            <Breadcrumbs
+                lastDisplayedItemsCount={LastDisplayedItemsCount.One}
+                firstDisplayedItemsCount={FirstDisplayedItemsCount.One}
+                items={['b', 'read', 'crumb', 's'].map((text) => ({ text, action: () => {} }))}
+            />
+            <Button>Btn</Button>
+        </ThemeProvider>
+    );
 }
 
 export default App;
